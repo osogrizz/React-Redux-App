67f3d1a98014d2cd4039ba39e57476491cbe3a8d
osogrizz <lhtor@gmail.com>
2019-11-23 21:12:58 -0800

    Adds components directory.

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { getSpaceX } from './actions';
+import Launches from './components/Launches'
 
 import './App.css';
 
@@ -10,36 +9,10 @@ function App(props) {
   return (
     <div className="App">
       <h1>Space X Upcoming Launches</h1>
-      <button onClick={() => props.getSpaceX() } >
-        Get Launches
-      </button>
-
-      {props.error && <div>{props.error}</div>}
-
-      {props.isLoading ? (
-        <div>Loading...</div> 
-      ) : (
-        props.launches.map( (launch, index) => (
-          <div key={index}>
-            <h2>Flight #{launch.flight_number}</h2>
-            <p>Mission Name: {launch.mission_name}</p>
-            <p>Launch Site: {launch.launch_site.site_name_long}</p>
-            {launch.details && <p>details: {launch.details}</p>}
-          </div>  
-        ))
-      )}
-
+      <Launches />
     </div>
   );
 }
 
-const mapStateToProps = state => {
-  return {
-    isLoading: state.isLoading,
-    error: state.error,
-    launches: state.launches
-    // launch: state.launch
-  }
-}
 
-export default connect(mapStateToProps, { getSpaceX })(App);
+export default App;
